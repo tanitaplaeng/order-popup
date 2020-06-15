ebf79b6c0a8cb218d678ca9e0dcd4f686ac4c9a0
tanita plaengprawat <tanitaplaeng@gmail.com>
2020-06-14 22:42:24 -0700

    custom popup

---
+++ custom-item.js
@@ -0,0 +1,26 @@
+// open and close popup
+let popupDiv = document.getElementById("popup-bg");
+
+function openPopupMenu() {
+    popupDiv.style.display = "block";
+}
+
+function closePopupMenu() { 
+    popupDiv.style.display = "none";
+}
+
+// quantity +/- increments
+function up(max) {
+    document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) + 1;
+    if (document.getElementById("myNumber").value >= parseInt(max)) {
+        document.getElementById("myNumber").value = max;
+    }
+}
+function down(min) {
+    document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) - 1;
+    if (document.getElementById("myNumber").value <= parseInt(min)) {
+        document.getElementById("myNumber").value = min;
+    }
+}
+
+
